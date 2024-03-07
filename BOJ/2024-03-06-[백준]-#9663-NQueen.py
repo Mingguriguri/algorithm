a711ff344a8436a80d09081aa4a183f7205be580
Mingguriguri <rlaalswjd-@naver.com>
2024-03-07 20:18:36 +0900

    BOJ #9663. NQueen 정답 코드

--- BOJ/2024-03-06-[백준]-#9663-NQueen.py
+++ BOJ/2024-03-06-[백준]-#9663-NQueen.py
@@ -1,34 +1,33 @@
+# pypy3로 통과
+
 import sys
 input = sys.stdin.readline
 
-def backtracking(n, board):
-    global cnt
-    print(board)
-    if len(board) == n:
-        return
-
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if j == board[i] or abs(j - i) == abs(i - j):
-                break
-            board.append[j]
-        cnt += backtracking(n, board + [i])
+# 유망한지 판단하는 함수
 def isPromissing(board, new):
-        for i in range(len(board)):
-            if new == board[i] or len(board)-i == abs(board[i]-new):
-                return False
-        return True
-    
-def nQueen(n, board):
-    if len(board) == n:
+        for i in range(new):
+
+            # 같은 열이면 안 되고, 대각선상에 있어서도 안 된다.
+            if board[new] == board[i] or new - i == abs(board[new]-board[i]):
+                return 0
         return 1
-    cnt = 0
+    
+# N Queen 알고리즘 수행
+def nQueen(new, board):
+    global n
+    global count
+    print(board)
+    # n 이 마지막행까지 수행하고 여기까지 오면 찾기 완료!
+    if new == n:
+        count += 1
+        return
     for i in range(n):
-        if isPromissing(board, i):
-            cnt += nQueen(n, board+[i])
-            
-    return cnt
-cnt = 0
+        board[new] = i
+        if isPromissing(board, new):
+            nQueen(new + 1, board)
+
+count = 0
+board = [0] * 15
 n = int(input())
-#backtracking(n, [])
-print(nQueen(n, []))
+nQueen(0, board)
+print(count)
