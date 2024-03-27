62bcb1466d3ec2917d573e309ded3306e7d6e103
Mingguriguri <rlaalswjd-@naver.com>
2024-03-28 00:05:29 +0900

    [BOJ] #1149. RGB거리 / 실버1 / 56분 (성공)

--- BOJ/2024-03-27-[백준]-#1149-RGB거리.py
+++ BOJ/2024-03-27-[백준]-#1149-RGB거리.py
@@ -1,12 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-a = [0]*n
-
+cost = []
 for i in range(n):
-    a[i] = list(map(int,input().split()))
-    
-for i in range(1,n): # 1부터 시작하는 이유는 다음 입력값이 이전 입력값의 최소값을 사용하기때문이다
-    a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
-    a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
-    a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
+    temp = list(map(int, input().split()))
+    cost.append(temp)
 
-print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
+# 0: RED, 1: GREEN, 2: BLUE
+for i in range(1, n):
+    cost[i][0] = min(cost[i-1][1], cost[i-1][2]) + cost[i][0] # RED
+    cost[i][1] = min(cost[i-1][0], cost[i-1][2]) + cost[i][1] # GREEN
+    cost[i][2] = min(cost[i-1][0], cost[i-1][1]) + cost[i][2] # BLUE
+
+print(min(cost[-1][0], cost[-1][1], cost[-1][2]))
