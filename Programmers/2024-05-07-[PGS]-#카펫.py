205232206aa3b3a28388af5956a03fa8c0589a3a
Mingguriguri <rlaalswjd-@naver.com>
2024-05-07 16:14:21 +0900

    docs: [PGS] 카펫에 대한 다른 사람 풀이도 코드에 추가

--- Programmers/2024-05-07-[PGS]-#카펫.py
+++ Programmers/2024-05-07-[PGS]-#카펫.py
@@ -7,4 +7,27 @@ def solution(brown, yellow):
             
             if (2*w + 2*h - 4 == brown) and ((w-2)*(h-2) == yellow): # 조건에 해당하는지 확인
                 return [max(w, h), min(w,h)] 
-                
+
+# # 다른 풀이 1            
+# def solution(brown, yellow):
+#     for i in range(1, int(yellow**(1/2))+1):
+#         if yellow % i == 0:
+#             if 2*(i + yellow//i) == brown-4:
+#                 return [yellow//i+2, i+2]
+            
+# # 다른 풀이2
+# import math
+# def solution(brown, yellow):
+#     w = ((brown+4)/2 + math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+#     h = ((brown+4)/2 - math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+#     return [w,h]
+
+# # 다른 풀이 3
+# def solution(brown, yellow):
+#     nm = brown + yellow
+#     for n in range(1, nm+1):
+#         if nm%n != 0:
+#             continue
+#         m = nm//n
+#         if (n-2)*(m-2) == yellow:
+#             return sorted([n, m], reverse = True)
