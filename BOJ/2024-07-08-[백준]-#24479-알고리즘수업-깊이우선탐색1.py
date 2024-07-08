68d814cb7cfb27475376068f9e2248ba227bfe1f
Mingguriguri <rlaalswjd-@naver.com>
2024-07-08 18:06:42 +0900

    [BOJ] #24479. 알고리즘 수업 - 깊이 우선 탐색 1 / 실버2 / 실패

---
+++ BOJ/2024-07-08-[백준]-#24479-알고리즘수업-깊이우선탐색1.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10**6) # 최대 재귀한도 깊이
+input = sys.stdin.readline
+
+def dfs(v):
+    global order
+    visited[v] = order  # 방문하면 순서 넣기
+    order += 1          # 다음 순서로 넘어가기
+    for u in sorted(graph[v]): # 오름차순으로 인접노드 방문하기 위해 정렬
+        if visited[u] == 0: # 방문 안 한 노드면 dfs탐색
+            dfs(u)
+
+# n: 정점의 수, m: 간선의 수, r: 시작 정점
+n, m, r = map(int, input().strip().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1) # 방문 순서 저장. 0이면 방문 X
+order = 1
+
+# m개의 간선 정보를 입력받아 그래프로 연결하기
+for _ in range(m):
+    u, v = map(int, input().strip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(r)
+
+# 해당노드를 몇 번째로 방문했는지 출력
+for i in range(1, n + 1):
+    print(visited[i])
