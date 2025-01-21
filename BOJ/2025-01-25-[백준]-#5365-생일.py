1520413ff3333d6f8d029bed3f9368888fcb3728
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-01-21 16:45:38 +0900

    [BOJ] #5365. 생일 / 실버5 / 구현, 문자열 / 15분 / 성공

--- BOJ/2025-01-25-[백준]-#5365-생일.py
+++ BOJ/2025-01-25-[백준]-#5365-생일.py
@@ -1,20 +1,15 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
 # 입력 받기
-N = int(input())  # 카드의 개수
-queue = deque(range(1, N + 1))  # 1부터 N까지 큐 생성
-answer = []  # 버린 카드를 저장할 리스트
+students = []
+for _ in range(int(input())):
+    name, day, month, year = input().split()
+    students.append((int(year), int(month), int(day), name))  # 튜플로 저장
 
-# 큐에서 카드 처리
-while len(queue) > 1:
-    answer.append(queue.popleft())  # 맨 앞의 카드를 버린다
-    queue.append(queue.popleft())  # 그다음 카드를 맨 뒤로 옮긴다
+# 정렬: 연도 -> 월 -> 일 기준
+students.sort()
 
-# 마지막 남은 카드 추가
-answer.append(queue.popleft())
-
-# 출력
-print(' '.join(map(str, answer)))  # 버린 카드와 마지막 남은 카드 출력
+# 가장 나이가 적은 사람과 많은 사람 출력
+print(students[-1][3])  # 가장 나이가 적은 사람
+print(students[0][3])  # 가장 나이가 많은 사람
